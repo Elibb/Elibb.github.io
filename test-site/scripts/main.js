8299f9bdd8c2fb73a6a52527dc705151e67ccb53
Shenghe Bi <bishenghe@126.com>
2017-06-14 22:04:20 +1000

    inite

---
+++ test-site/scripts/main.js
@@ -0,0 +1,8 @@
+// var myHeading = document.querrySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+
+var myHeading = document.querySelector('h1');
+console.log(myHeading);
+console.log(123123);
+myHeading.textContent = 'Hello world!';
